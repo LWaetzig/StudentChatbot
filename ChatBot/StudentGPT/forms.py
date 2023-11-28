bc160e04f1624bbff2c7e11401f964fc5966c2ca
LWaetzig <lucas@waetzig.net>
2023-11-28 14:30:03 +0100

    wip

---
+++ ChatBot/StudentGPT/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class FileUpload(forms.Form):
+    file = forms.FileField()
+    
---
+++ ChatBot/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./StudentGPT/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
