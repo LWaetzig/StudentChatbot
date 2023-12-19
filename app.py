002c6270c2da7a761dc7e2241e87214579bbe735
LWaetzig <lucas@waetzig.net>
2023-12-19 14:57:09 +0100

    update chatbot interface

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+import time
+
 import streamlit as st
 from streamlit_extras.add_vertical_space import add_vertical_space
+
+from utils.Chatbot import Chatbot
+from utils.FileProcessor import FileProcessor
 
 # site config
 st.set_page_config(
@@ -16,7 +21,9 @@ st.set_page_config(
 
 # Session states
 if "messages" not in st.session_state:
-    st.session_state.messages = []
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Hello, I am JARVIS. How can I help you?"}
+    ]
 
 
 # site content
@@ -24,6 +31,46 @@ with st.sidebar:
     st.title("JARVIS")
     st.caption("Just A Rather Very Intelligent Student")
 
-    add_vertical_space(5)
-    uploaded_file = st.file_uploader("Datei auswählen", type=["pdf", "txt"])
-    
+    add_vertical_space(2)
+
+    model = st.selectbox(
+        "Model",
+        ("GPT-3", "LLama 2", "BERT"),
+        index=None,
+        placeholder="Select a model...",
+        disabled=True,
+    )
+
+    add_vertical_space(2)
+    uploaded_file = st.file_uploader("Datei auswählen", type=["pdf"])
+    if uploaded_file is not None:
+        print(uploaded_file)
+        FileProcessor = FileProcessor(uploaded_file)
+        with st.spinner("Processing file..."):
+            FileProcessor.process()
+            st.success("Done!")
+
+
+# display stored messages
+for message in st.session_state.messages:
+    avatar = "🤖" if message["role"] == "assistant" else "🧑‍💻"
+    with st.chat_message(name=message["role"], avatar=avatar):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message(name="user", avatar="🧑‍💻"):
+        st.write(prompt)
+
+    # generate response
+    response = Chatbot.generate_response(prompt)
+    st.session_state.messages.append({"role": "jarvis", "content": response})
+    with st.chat_message(name="assistant", avatar="🤖"):
+        message_placeholder = st.empty()
+        full_response = ""
+
+        for chunk in response.split():
+            full_response += chunk + " "
+            time.sleep(0.1)
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
