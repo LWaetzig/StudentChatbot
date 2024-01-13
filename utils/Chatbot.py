ca54362d191accd6d8d51c46a9e1084968b97e94
LWaetzig <lucas@waetzig.net>
2024-01-13 14:27:33 +0100

    change the process how the different models are called

--- utils/Chatbot.py
+++ utils/Chatbot.py
@@ -1,36 +1,25 @@
-import random
 import requests
 
+
 class Chatbot:
+    def __init__(self, model: str, api_token: str) -> None:
+        self.API_TOKEN = api_token
+        self.model = model
+        if self.model == "T5":
+            self.API_URL = "https://api-inference.huggingface.co/models/t5-large"
+        elif self.model == "BART":
+            self.API_URL = (
+                "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+            )
 
-    def __init__(self) -> None:
-
-         # +++A valid huggingface token is needed here+++
-        self.API_TOKEN = "YOUR_HUGGINGFACE_ACCESS_TOKEN" 
-        pass
-
-    def generate_response(self, message) -> str:
-        response = random.choice(
-            [
-                "I don't know what you mean.",
-                "I don't understand.",
-                "I'm sorry, I don't know what you mean.",
-                "I'm sorry, I don't understand.",
-            ]
-        )
-        return response
-
-    def call_T5(self, message) -> str:
-        API_URL = "https://api-inference.huggingface.co/models/t5-large"
-
+    def generate_response(self, message: str) -> str:
         headers = {"Authorization": f"Bearer {self.API_TOKEN}"}
-        response = requests.post(API_URL, headers = headers, json = message)
-        return [response.json(), "translation_text"]    
-    
-
-    def call_BART(self, message) -> str:
-        API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
-
-        headers = {"Authorization": f"Bearer {self.API_TOKEN}"}
-        response = requests.post(API_URL, headers = headers, json = message)
-        return [response.json(), "summary_text"]
+        message = {"inputs": message, "parameters": {"num_beams": 3}}
+        response = requests.post(self.API_URL, headers=headers, json=message)
+        if response.status_code == 200:
+            if self.model == "T5":
+                return response[0].get("translation_text")
+            elif self.model == "BART":
+                return response[0].get("summary_text")
+        else:
+            return "Error: Could not generate response. Maybe check your API Token?"
