9dffb080e0a84f57f3c5d0460863f840eaea1c14
Nicho Link <nicholaselink@gmail.com>
2024-01-21 19:20:18 +0100

    fix typo

--- utils/Chatbot.py
+++ utils/Chatbot.py
@@ -13,7 +13,7 @@ class Chatbot:
         )
         self.prompt = PromptTemplate(
             input_variables=["question", "context"],
-            template="You are a professor teaching a class. A student asks you '{question}'. You response is based on the fact that: '{context}'",
+            template="You are a professor teaching a class. A student asks you '{question}'. Your response is based on the fact that: '{context}'",
         )
         self.chain = LLMChain(prompt=self.prompt, llm=self.llm, verbose=True)
 
