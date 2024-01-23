3f4bb2c38e43673c0907d4d5465df4397c74a5e0
LWaetzig <lucas@waetzig.net>
2024-01-23 14:58:45 +0100

    add comments, final cleanup and formatting

--- utils/Chatbot.py
+++ utils/Chatbot.py
@@ -5,9 +5,11 @@ from langchain.prompts import PromptTemplate
 
 class Chatbot:
     def __init__(self, **kgwars):
+        # set hugging face model
         self.llm = HuggingFaceHub(
             repo_id="tiiuae/falcon-7b-instruct", model_kwargs=kgwars
         )
+        # create prompt
         self.prompt = PromptTemplate(
             input_variables=["question", "context"],
             template="You are a professor teaching a class. A student asks you '{question}'. Your response is based on the fact that: '{context}'",
